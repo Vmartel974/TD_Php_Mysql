2510d099690d1eeff9483345ebb1dee531bcfc4d
Vmartel974 <vmartel@la-providence.net>
2020-12-17 12:51:49 +0100

    avancement

---
+++ menu.php
@@ -0,0 +1,61 @@
+<?php
+    function menu(){
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul id="menu-demo2">
+        <li><a href="#">PHP Partie 1</a>
+            <ul>
+                <li><a href="#">lien sous menu 1</a></li>
+                <li><a href="#">lien sous menu 1</a></li>
+                <li><a href="#">lien sous menu 1</a></li>
+                <li><a href="#">lien sous menu 1</a></li>
+            </ul>
+        </li>
+        <li><a href="#">PHP Partie 2</a>
+            <ul>
+                <li><a href="#">Lien sous menu 2</a></li>
+                <li><a href="#">Lien sous menu 2</a></li>
+                <li><a href="#">Lien sous menu 2</a></li>
+                <li><a href="#">Lien sous menu 2</a></li>
+            </ul>
+        </li>
+        <li><a href="#">BDD</a>
+            <ul>
+                <li><a href="#">lien sous menu 3</a></li>
+                <li><a href="#">lien sous menu 3</a></li>
+                <li><a href="#">lien sous menu 3</a></li>
+                <li><a href="#">lien sous menu 3</a></li>
+            </ul>
+        </li>
+        <li><a href="#">SQL</a>
+            <ul>
+                <li><a href="#">Lien sous menu 4</a></li>
+                <li><a href="#">Lien sous menu 4</a></li>
+                <li><a href="#">Lien sous menu 4</a></li>
+                <li><a href="#">Lien sous menu 4</a></li>
+            </ul>
+        </li>
+        <li><a href="#">HTML/CSS</a>
+            <ul>
+                <li><a href="html/exo1.html">Exercice 1</a></li>
+                <li><a href="html/exo2.html">Exercice 2</a></li>
+                <li><a href="html/exo3.html">Exercice 3</a></li>
+                <li><a href="html/formulaire1.html">Formulaire</a></li>
+            </ul>
+    </ul>
+</body>
+
+</html>
+
+<?php
+    }
+?>
