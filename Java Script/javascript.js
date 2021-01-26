ecdb9ce62285fa4324640bcb4bb888614edbfb34
Vmartel974 <vmartel@la-providence.net>
2021-01-26 17:49:35 +0100

    854

--- Java Script/javascript.js
+++ Java Script/javascript.js
@@ -4,7 +4,7 @@ function surfaceRectangle() {
     var longueur;
     largeur = prompt("Quelle est la largeur du rectangle ?");
     longueur = prompt("Quelle est la longueur du rectangle ?");
-    alert("La surface vaut " + longueur * largeur);
+    alert("L'aire vaut " + longueur * largeur);
     alert("Le périmètre vaut " + 2 * (Number(longueur) + Number(largeur)));
 }
 
@@ -12,8 +12,19 @@ function surfaceRectangle() {
 function surfaceCercle() {
     var longueur = parseFloat(prompt("Entrez la longueur du cercle : "));
     var rayon = parseFloat(prompt("Entrez le rayon du cercle : "));
-    alert("L'air du cercle vaut " + 3.14 * (Number(longueur) * Number(longueur)));
-    alert("Le périmètre du cercle vaut " + Number(rayon) * 2 * 3.14) ;
+    alert("L'aire du cercle vaut " + 3.14 * (Number(longueur) * Number(longueur)) + " cm²");
+    alert("Le périmètre du cercle vaut " + Number(rayon) * 2 * 3.14 + " cm");
+}
 
+var a = 3;
+var b = 2;
 
-}   
+function multiplie(x = 8) {
+    return 3 * x;
+}
+
+function affiche() {
+    alert(multiplie(a));
+    alert(multiplie(b));
+    alert(multiplie())
+}
