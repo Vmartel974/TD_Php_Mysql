321ee9ab40076f4000c8074db6ae921b58cadcaf
Vmartel974 <vmartel@la-providence.net>
2021-02-09 14:58:16 +0100

    exo8 finis

--- Java Script/javascript.js
+++ Java Script/javascript.js
@@ -72,3 +72,51 @@ function affiche3() {
     alert(soustrait(tab[tab.length - 3]));
     alert(soustrait(tab[tab.length - 1]));
 }
+
+//Jour de la semaine
+function jourDeLaSemaine() {
+    var aujourdhui = new Date;
+    jour = aujourdhui.getDay();
+    var jourFrancais;
+    switch (jour) {
+        case 0:
+            jourFrancais = "dimanche";
+            break;
+        case 1:
+            jourFrancais = "lundi";
+            break;
+        case 2:
+            jourFrancais = "mardi";
+            break;
+        case 3:
+            jourFrancais = "mercredi";
+            break;
+        case 4:
+            jourFrancais = "jeudi";
+            break;
+        case 5:
+            jourFrancais = "vendredi";
+            break;
+        case 6:
+            jourFrancais = "samedi";
+            break;
+        default:
+            jourFrancais = "jour inexistant";
+            break;
+    }
+    alert("Nous sommes " + jourFrancais + ".");
+}
+
+//changement de couleurs 
+function couleurs1(){
+    document.getElementById("demo").style.color = "lime";
+}
+function couleurs2(){
+    document.getElementById("demo").style.color = "navy";
+}
+function couleurs3(){
+    document.getElementById("demo").style.color = "black";
+}
+function couleurs4(){
+    document.getElementById("demo").style.color = "red";
+}
